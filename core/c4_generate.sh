99dda6e311c07997830201a1de21de020e60d295
pablicio <pabliciotjg@gmail.com>
2023-09-06 19:55:24 -0300

    Add new steps to ansible provision

--- core/c4_generate.sh
+++ core/c4_generate.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-`git fetch 2>/dev/null`;
+# #!/bin/bash
+# `git fetch 2>/dev/null`;
 
-for i in `git diff --name-only main origin/main 2>/dev/null | egrep '\.(puml)$'`; do
-  # generated images from .puml file
-  echo $i;
-  # `java -jar plantuml.jar $i`;
-done
+# for i in `git diff --name-only main origin/main 2>/dev/null | egrep '\.(puml)$'`; do
+#   # generated images from .puml file
+#   echo $i;
+#   # `java -jar plantuml.jar $i`;
+# done
   
 ## generate current folder structure and save in json
-`rm map_folder.json 2> /dev/null`;
-`python3 map_formatter.py >> map_folder.json`;
+`rm services/map_folder.json 2> /dev/null`;
+`python3 map_formatter.py >> services/map_folder.json`;
