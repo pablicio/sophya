2b3a5891cb94ed35d79aa7214f5749c9b60d6752
pablicio <pabliciotjg@gmail.com>
2023-09-09 08:50:48 -0300

    fix scripts

--- core/c4_generate.sh
+++ core/c4_generate.sh
@@ -1,11 +1,11 @@
 # #!/bin/bash
 # `git fetch 2>/dev/null`;
 
-# for i in `git diff --name-only main origin/main 2>/dev/null | egrep '\.(puml)$'`; do
-#   # generated images from .puml file
-#   echo $i;
-#   # `java -jar plantuml.jar $i`;
-# done
+for i in `git diff --name-only main origin/main 2>/dev/null | egrep '\.(puml)$'`; do
+  # generated images from .puml file
+  echo $i;
+  # `java -jar plantuml.jar $i`;
+done
   
 ## generate current folder structure and save in json
 `rm ../docs/map_folder.json 2>/dev/null`;
--- core/sync_repo.sh
+++ core/sync_repo.sh
@@ -5,7 +5,7 @@
 # $4  = file-check
 # example sh sync_repo.sh my-repo-name user-git token-git README.md
 
-if [[ -f /home/ubuntu/repos/$1/$4 ]]; then
+if [[ -f /home/ubuntu/repos/$1/$4 2>/dev/null ]]; then
   cd /home/ubuntu/repos/$1
   git fetch
   rm /home/ubuntu/docs/diff.json
@@ -14,4 +14,7 @@ else
   git config user.name $2 2>/dev/null
   git config github.token $3 2>/dev/null
   git clone https://github.com/$2/$1.git /home/ubuntu/repos/$1 2>/dev/null
+  rm /home/ubuntu/docs/diff.json
+  find /home/ubuntu/repos/$1/sophya-docs -type f  -name "*.puml" -mmin -60 >> /home/ubuntu/docs/diff.json
 fi
+
