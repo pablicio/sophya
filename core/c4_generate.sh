a6b8e6bff3f54b83e34187f5e73c6b4b5ee66a7b
pablicio <pabliciotjg@gmail.com>
2023-09-09 14:31:12 -0300

    Test diff

--- core/c4_generate.sh
+++ core/c4_generate.sh
@@ -1,11 +1,11 @@
 # #!/bin/bash
 # `git fetch 2>/dev/null`;
 
-for i in `sudo /home/ubuntu/docs/diff.json`; do
+for i in `/home/ubuntu/docs/diff.json`; do
   # generated images from .puml file
-  `sudo java -jar plantuml.jar $i`;
+  `java -jar plantuml.jar $i`;
 done
   
 ## generate current folder structure and save in json
-`sudo rm /home/ubuntu/docs/map_folder.json 2>/dev/null`;
-`sudo python3 map_formatter.py 2>/dev/null >> /home/ubuntu/docs/map_folder.json`;
+`rm /home/ubuntu/docs/map_folder.json 2>/dev/null`;
+`python3 map_formatter.py 2>/dev/null >> /home/ubuntu/docs/map_folder.json`;
