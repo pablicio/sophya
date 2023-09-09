09cd0d9de9d27d2ee6a6f876a1c8667e61a34fb9
pablicio <pabliciotjg@gmail.com>
2023-09-09 14:27:09 -0300

    Test diff

--- core/sync_repo.sh
+++ core/sync_repo.sh
@@ -15,6 +15,7 @@ else
   git config github.token $3 2>/dev/null
   git clone https://github.com/$2/$1.git /home/ubuntu/repos/$1 2>/dev/null
   chmod 744 -R /home/ubuntu/repos/$1/
+  chmod 744 -R /home/ubuntu/docs/
   rm /home/ubuntu/docs/diff.json
   find /home/ubuntu/repos/$1/sophya-docs -type f  -name "*.puml" -mmin -60 >> /home/ubuntu/docs/diff.json
 fi
