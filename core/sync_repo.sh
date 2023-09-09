4252146f0c82e1bf493e1fa9c6b918ebb7542bcd
pablicio <pabliciotjg@gmail.com>
2023-09-09 14:39:07 -0300

    Test diff

--- core/sync_repo.sh
+++ core/sync_repo.sh
@@ -10,13 +10,14 @@ if [[ -f /home/ubuntu/repos/$1/$4 2>/dev/null ]]; then
   git fetch
   rm /home/ubuntu/docs/diff.json
   git diff --name-only origin/main | egrep '\.(puml)$' >> /home/ubuntu/docs/diff.json
+  chmod 744 /home/ubuntu/docs/diff.json
 else
   git config user.name $2 2>/dev/null
   git config github.token $3 2>/dev/null
   git clone https://github.com/$2/$1.git /home/ubuntu/repos/$1 2>/dev/null
-  chmod 744 -R /home/ubuntu/repos/$1/
-  chmod 744 -R /home/ubuntu/docs/
   rm /home/ubuntu/docs/diff.json 2>/dev/null
   find /home/ubuntu/repos/$1/sophya-docs -type f  -name "*.puml" -mmin -60 >> /home/ubuntu/docs/diff.json
+  chmod 744 -R /home/ubuntu/repos/$1/
+  chmod 744 /home/ubuntu/docs/diff.json
 fi
 
