fd86868b33fe9de5e3131033458989cf5b7e4d7c
pablicio <pabliciotjg@gmail.com>
2023-09-09 08:05:43 -0300

    Add sync files scripts

---
+++ core/sync_docs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# $1 = my-repo-name
+# $2  = user-git
+# $3  = token-git
+# $4  = file-check
+# example sh sync_docs.sh my-repo-name
+
+cd /home/ubuntu/repos/$1
+git pull
+rsync -rltgoDzvO --delete /home/ubuntu/repos/$1/sophya-docs/ /home/ubuntu/docs/$1/
---
+++ core/sync_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# $1 = my-repo-name
+# $2  = user-git
+# $3  = token-git
+# $4  = file-check
+# example sh sync_repo.sh my-repo-name user-git token-git README.md
+
+if [[ -f /home/ubuntu/repos/$1/$4 ]]; then
+  cd /home/ubuntu/repos/$1
+  git fetch
+  rm /home/ubuntu/docs/diff.json
+  git diff --name-only origin/main | egrep '\.(puml)$' >> /home/ubuntu/docs/diff.json
+else
+  git config user.name $2
+  git config github.token $3
+  git clone https://github.com/$2/$1.git /home/ubuntu/repos/$1 2>/dev/null
+fi
