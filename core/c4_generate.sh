fa437e6accaf0eea1504da5deafe6181f3073218
pablicio <pabliciotjg@gmail.com>
2023-09-09 08:53:45 -0300

    fix scripts

--- core/c4_generate.sh
+++ core/c4_generate.sh
@@ -1,12 +1,11 @@
 # #!/bin/bash
 # `git fetch 2>/dev/null`;
 
-for i in `git diff --name-only main origin/main 2>/dev/null | egrep '\.(puml)$'`; do
+for i in `/home/ubuntu/docs/diff.json`; do
   # generated images from .puml file
-  echo $i;
-  # `java -jar plantuml.jar $i`;
+  `java -jar plantuml.jar $i`;
 done
   
 ## generate current folder structure and save in json
-`rm ../docs/map_folder.json 2>/dev/null`;
-`python3 map_formatter.py 2>/dev/null >> ../docs/map_folder.json`;
+`rm /home/ubuntu/docs/map_folder.json 2>/dev/null`;
+`python3 map_formatter.py 2>/dev/null >> /home/ubuntu/docs/map_folder.json`;
