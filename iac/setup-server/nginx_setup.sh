7ca57d740a7b3d726bc45c018b5c9c86a966574e
pablicio <pabliciotjg@gmail.com>
2023-09-09 20:25:20 -0300

    Fix files nginx

--- iac/setup-server/nginx_setup.sh
+++ iac/setup-server/nginx_setup.sh
@@ -23,19 +23,20 @@ EOF
 
 cat > sophya_web << EOF
 server {
-    listen      8001;
-    server_name sophya_web_app;
-    charset utf-8;
-    root    /var/www/sophya-web/dist;
-    index   index.html index.htm;
-    # Always serve index.html for any request
-    location / {
-        root /var/www/sophya-web/dist;
-        try_files $uri /index.html;
-    }
-    error_log  /var/log/nginx/vue-app-error.log;
-    access_log /var/log/nginx/vue-app-access.log;
-}
+		listen 80;
+		server_name mysite.com;
+
+		charset utf-8;
+		root /var/www/sophya-web/dist;
+		index index.html index.htm;
+
+		location / {
+			root /var/www/sophya-web/dist;
+			try_files $uri $uri/ /index.html;
+		}
+
+		include  /etc/nginx/mime.types;
+	}
 EOF
 
 sudo cp -R doc_server /etc/nginx/sites-available/doc_server
